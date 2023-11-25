d428c0dde0bf1ad255924bcad54c124af61c1c3d
CipherXenon <151166826+CipherXenon@users.noreply.github.com>
2023-11-25 17:59:13 +0600

    Create button.js

---
+++ button.js
@@ -0,0 +1,9 @@
+// JavaScript code for the Next Page button
+const nextPageButton = document.querySelector('button');
+
+nextPageButton.addEventListener('click', () => {
+    // Perform the action to go to the next page here
+    // For now, let's simulate navigating to the next page by logging a message
+    console.log('Navigating to the next page...');
+    window.location.href = 'system_software.html';
+});
